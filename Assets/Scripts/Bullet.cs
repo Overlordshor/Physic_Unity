986397c832273b76da65f22aeca0f2c481a332b7
Дмитрий Шаров <overlord.shor@gmail.com>
2021-03-02 10:59:14 +0300

    Рефакторинг, попадение по объекту добавляет след от пули

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public Action OnHit;
+    [SerializeField] private GameObject _markPrefab;
     private Rigidbody _rigidbody;
     private bool _isActive = true;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        Destroy(gameObject, 5f);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -18,18 +19,37 @@ public class Bullet : MonoBehaviour
         {
             _rigidbody.useGravity = true;
             Debug.Log(collision.gameObject.name);
-            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
-            if (enemy)
-            {
-                enemy.OnHit();
-            }
+
+            OnHitGameObject(collision);
+
+            _isActive = false;
+        }
+    }
+
+    private void OnHitGameObject(Collision collision)
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy)
+        {
+            enemy.OnHit();
             Head head = collision.collider.GetComponent<Head>();
             if (head)
             {
                 Debug.Log(head + "SHOT");
             }
-
-            _isActive = false;
         }
+        else
+        {
+            AddMark(collision);
+        }
+    }
+
+    private void AddMark(Collision collision)
+    {
+        // Позиция первого контакта;
+        var positionMark = collision.contacts[0].point;
+        // Поворот метки. Получаем кватернион из вектора поверхности (нормали);
+        var rotationMark = Quaternion.LookRotation(collision.contacts[0].normal);
+        Instantiate(_markPrefab, positionMark, rotationMark, collision.transform);
     }
 }
